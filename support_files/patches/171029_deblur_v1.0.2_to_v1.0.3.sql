2d13b86e1e0daa72350bd139dafc2f50f3ac115c
Antonio Gonzalez <antgonza@gmail.com>
2017-10-30 10:28:06 -0600

    adding the new patch, cc: @josenavas

--- support_files/patches/171029_deblur_v1.0.2_to_v1.0.3.sql
+++ support_files/patches/171029_deblur_v1.0.2_to_v1.0.3.sql
@@ -18,7 +18,6 @@ BEGIN
             JOIN qiita.software s USING (software_id)
         WHERE s.name = 'deblur' AND s.version = '1.0.3' AND sc.name = 'deblur-workflow';
 
-    -- Modify those artifacts and jobs that have been picked against Greengenes
     FOR a_info IN
         SELECT * FROM qiita.artifact WHERE command_id = old_deblur_id
     LOOP
@@ -36,4 +35,45 @@ BEGIN
             WHERE artifact_id = a_info.artifact_id;
 
     END LOOP;
+END $do$
+
+-- after initial deploy we realized that we need to make sure that all
+-- pos-ref-db-fp  /databases/gg/13_8/sortmerna/88_otus
+-- pos-ref-fp     /databases/gg/13_8/rep_set/88_otus.fasta
+
+DO $do$
+DECLARE
+    deblur_id BIGINT;
+    a_info        RECORD;
+    pj_id         UUID;
+BEGIN
+    SELECT command_id INTO deblur_id
+        FROM qiita.software_command sc
+            JOIN qiita.software s USING (software_id)
+        WHERE s.name = 'deblur' AND s.version = '1.0.3' AND sc.name = 'deblur-workflow';
+
+    FOR a_info IN
+        SELECT * FROM qiita.artifact WHERE command_id = deblur_id
+    LOOP
+        SELECT processing_job_id INTO pj_id
+            FROM qiita.processing_job
+                JOIN qiita.artifact_output_processing_job USING (processing_job_id)
+            WHERE artifact_id = a_info.artifact_id;
+
+        UPDATE qiita.processing_job
+            SET (parameters->>'pos-ref-db-fp' = '/databases/gg/13_8/sortmerna/88_otus',
+                 parameters->>'pos-ref-fp' = '/databases/gg/13_8/rep_set/88_otus.fasta')
+            WHERE processing_job_id = pj_id;
+
+        UPDATE qiita.artifact
+            SET (parameters->>'pos-ref-db-fp' = '/databases/gg/13_8/sortmerna/88_otus',
+                 parameters->>'pos-ref-fp' = '/databases/gg/13_8/rep_set/88_otus.fasta')
+            WHERE artifact_id = a_info.artifact_id;
+
+    END LOOP;
+
+    UPDATE qiita.default_parameter_set
+      SET (parameters->>'pos-ref-db-fp' = '/databases/gg/13_8/sortmerna/88_otus',
+           parameters->>'pos-ref-fp' = '/databases/gg/13_8/rep_set/88_otus.fasta')
+      WHERE command_id = deblur_id;
 END $do$
