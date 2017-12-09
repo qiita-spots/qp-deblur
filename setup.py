aa845daa6172752420d3094efbd7f9cc1488317b
sjanssen2 <stefan.m.janssen@gmail.com>
2017-12-09 12:25:22 -0800

    flake8

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ setup(name='qp-deblur',
       extras_require={'test': ["nose >= 0.10.1", "pep8"]},
       install_requires=['click >= 3.3', 'future', 'deblur'],
       dependency_links=[
-        ('https://github.com/qiita-spots/qiita-files/archive/master.zip#'
-         'egg=qiita-files-0.1.0-dev')],
+          ('https://github.com/qiita-spots/qiita-files/archive/master.zip#'
+           'egg=qiita-files-0.1.0-dev')],
       classifiers=classifiers,
       cmdclass={'install': PostInstallCommand})
